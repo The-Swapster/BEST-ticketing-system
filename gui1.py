b80542a36c6bade47c82b7d58b93a06b86e768ce
The-Swapster <51404379+The-Swapster@users.noreply.github.com>
2020-03-25 19:54:42 +0530

    Update gui1.py

--- gui1.py
+++ gui1.py
@@ -1,12 +1,8 @@
 import tkinter as tk
-import database_1 as db
+import database as db
 import sqlite3
 
-root = tk.Tk()
-root.geometry("325x200")
-root.title("Choose Bus")
-counter=tk.IntVar()
-list1 =[
+spl1 = [
     "Mumbai CST",
     "Hutatma Chowk",
     "Ahilyabai Holkar Chowk",
@@ -37,40 +33,47 @@ list1 =[
     "10th Road Chembur Church",
     "Dr. Ambedkar Garden"
 ]
-variable = tk.StringVar(root)
-variable.set(list1[0])
-variable1 = tk.StringVar(root)
-variable1.set(list1[0])
 
-def show_entry_fields():
+
+def show_entry_fields(v1, v2, a):
     t = tk.Tk()
     t.title("Ticket details")
-    s=''
-    a=counter.get()
+    s = ''
     for i in range(a):
-        l=db.data_entries_passenger(variable.get(),variable1.get())
+        l = db.data_entries_passenger(v1, v2)
         a = "Select * from passenger where ticket_id = ?"
-        db.c.execute(a,(l,))
+        db.c.execute(a, (l,))
         r = db.c.fetchone()
-        s += "Bus Number:"+ str(r[0])+ "\nRoute Number: "+ str(r[1])+ "\nTicket Id :"+ str(r[2])+ "\nStart Stop: "+ str(r[3])+ "\nEnd Stop: "+str(r[4])+ "\nType: "+str(r[5])+"\n\n"
+        s += "Bus Number:" + str(r[0]) + "\nRoute Number: " + str(r[1]) + "\nTicket Id :" + str(
+            r[2]) + "\nStart Stop: " + str(r[3]) + "\nEnd Stop: " + str(r[4]) + "\nType: " + str(r[5]) + "\n\n"
     tk.Label(t, text=s).grid(row=0, column=0)
     t.mainloop()
 
-button1 = tk.Button(text='Submit', command=show_entry_fields)
-button1.grid(row=8, column=1, pady=4)
-text3 = tk.Label(root, text="Source")
-text4 = tk.Label(root, text="Destination")
-text5 = tk.Label(root, text="Number of people")
-w = tk.OptionMenu(root, variable, *list1)
-w.grid(row=0, column=4)
-x = tk.OptionMenu(root, variable1, *list1)
-x.grid(row=1, column=4)
-text3.grid(row=0, column=0)
-text4.grid(row=1, column=0)
-text5.grid(row=2, column=0)
-tk.Label(root, text="Number of people").grid(row=2, column=0)
-tk.Label(root, textvariable=counter).grid(row=2, column=5)
-tk.Button(root, text="+", command=lambda: counter.set(counter.get()+1)).grid(row=2,column=6)
-tk.Button(root, text="-", command=lambda: counter.set(counter.get()-1)).grid(row=2,column=4)
-root.mainloop()
 
+def call_gui1():
+    root = tk.Tk()
+    root.geometry("325x200")
+    root.title("Choose Bus")
+    counter = tk.IntVar(root)
+    variable = tk.StringVar(root)
+    variable.set(spl1[0])
+    variable1 = tk.StringVar(root)
+    variable1.set(spl1[0])
+    button1 = tk.Button(text='Submit', command=show_entry_fields(variable.get(), variable1.get(), counter.get()))
+    button1.grid(row=8, column=1, pady=4)
+    text3 = tk.Label(root, text="Source")
+    text4 = tk.Label(root, text="Destination")
+    text5 = tk.Label(root, text="Number of people")
+    w = tk.OptionMenu(root, variable, *spl1)
+    w.grid(row=0, column=4)
+    x = tk.OptionMenu(root, variable1, *spl1)
+    x.grid(row=1, column=4)
+    text3.grid(row=0, column=0)
+    text4.grid(row=1, column=0)
+    text5.grid(row=2, column=0)
+    tk.Label(root, text="Number of people").grid(row=2, column=0)
+    tk.Label(root, textvariable=counter).grid(row=2, column=5)
+    tk.Button(root, text="+", command=lambda: counter.set(counter.get() + 1)).grid(row=2, column=6)
+    tk.Button(root, text="-", command=lambda: counter.set(counter.get() - 1)).grid(row=2, column=4)
+
+    root.mainloop()
