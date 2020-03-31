6a7d20ef3f95b13738fea1856e9120264ee9bdc4
The-Swapster <51404379+The-Swapster@users.noreply.github.com>
2020-03-31 10:43:03 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,20 +1,26 @@
 from tkinter import *
+import sqlite3
 import gui1
 import gui2
 import database
 
-try:
-    t = Tk()
-    t.title('BEST Ticketing System')
-    database.calls()
-    Button(t, text='Conductor', command=gui2.call_gui2).pack()
-    Button(t, text='Passenger', command=gui1.call_gui1).pack()
-    t.mainloop()
 
-except:
+def gui():
     t = Tk()
     t.title('BEST Ticketing System')
-    Button(t, text='Conductor', command=gui2.call_gui2()).pack()
-    Button(t, text='Passenger', command=gui1.call_gui1()).pack()
+    b1 = Button(t, text='Conductor', command=gui2.call_gui2)
+    b1.grid(row=0)
+    b2 = Button(t, text='Passenger', command=gui1.call_gui1)
+    b2.grid(row=1)
     t.mainloop()
+    
+
+try:
+    database.calls()
+    gui()
+
+
+except sqlite3.IntegrityError:
+    gui()
+
 
