5ad01f0257959d26607c1832b29ed86e3228b206
The-Swapster <51404379+The-Swapster@users.noreply.github.com>
2020-04-02 03:09:15 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,20 +5,20 @@ import gui2
 import database
 
 
-def gui():
+def gui(v1):
     t = Tk()
     t.title('BEST Ticketing System')
-    b1 = Button(t, text='Conductor', command=gui2.call_gui2)
+    b1 = Button(t, text='Conductor', command=lambda :gui2.call_gui2(v1))
     b1.grid(row=0)
-    b2 = Button(t, text='Passenger', command=gui1.call_gui1)
+    b2 = Button(t, text='Passenger', command=lambda :gui1.call_gui1(v1))
     b2.grid(row=1)
-    t.mainloop()
-    
-
-try:
-    database.calls()
-    gui()
+    #t.mainloop()
 
 
-except sqlite3.IntegrityError:
-    gui()
+def form(v):
+    try:
+        database.calls()
+        gui(v)
+
+    except sqlite3.IntegrityError:
+        gui(v)
