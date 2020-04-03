eaaaacebf60210269441bc7b0235c3cc53c0bab3
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-04 02:04:24 +0530

    Update gui 4.py

--- gui 4.py
+++ gui 4.py
@@ -1,9 +1,5 @@
 from tkinter import *
-import sqlite3
 import gui
-import gui1
-import gui2
-import database
 
 route_no = ['1 SPL', 'C6Exp']
 t4 = Tk()
@@ -11,8 +7,6 @@ v3 = StringVar(t4)
 v3.set(route_no[0])
 w3 = OptionMenu(t4, v3, *route_no)
 w3.grid(row=0)
-b = Button(t4, text='Next', command=lambda: gui.form(v3.get()))
+b = Button(t4, text='Next', command=lambda: [gui.form(v3.get())])
 b.grid(row=1)
-b1 = Button(t4, text='Close', command=t4.destroy)
-b1.grid(row=2)
 t4.mainloop()
