edae7bdf2b7dbba31934f21e1c78a267c814a259
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-04 02:02:56 +0530

    Update gui2.py

--- gui2.py
+++ gui2.py
@@ -1,8 +1,6 @@
 from tkinter import *
-from tkinter import messagebox as m
 import database as db
 import sqlite3
-from functools import partial
 
 stops1Spl = [
     "Mumbai CST",
@@ -43,7 +41,6 @@ stopsC6Exp = [
     "Dr. Ambedkar Garden"
 ]
 
-
 spl1 = ["MH01CD1234",
         "MH01XY6754",
         "MH01MD3245",
@@ -59,23 +56,26 @@ C6Exp = ["MH01RM5018",
          ]
 
 
-def update_status(i1, i2):
+def update_status(v, v1, v2):
     try:
-        db.c.execute('Insert into bus_status(bus_number,current_location) Values(?,?)', (i1, i2))
-        db.c.execute('delete from passenger where end_stop = ?', (i2,))
+        db.c.execute("select sum(number) from passenger where end_stop = ?", (v2,))
+        count = db.c.fetchone()
+        print(count)
+        db.c.execute("select count from passenger_count where bus_number = ?", (v1,))
+        count1 = db.c.fetchone()
+        db.c.execute("update passenger_count set count = ? where bus_number = ?", (count1[0] - count[0], v1))
+        db.c.execute('delete from passenger where end_stop = ?', (v2,))
+        db.c.execute('Insert into bus_status(bus_number, current_location, passenger_count) Values(?,?,?)', (v1, v2, count1[0] - count[0]))
         db.connection.commit()
     except sqlite3.IntegrityError:
-        db.c.execute('update bus_status set current_location = ? where bus_number = ?', (i2, i1))
+        db.c.execute('update bus_status set current_location = ? where bus_number = ?', (v2, v1))
+        db.c.execute("select count(number) from passenger where end_stop = ?", (v2,))
+        count = db.c.fetchone()
+        db.c.execute("select count from passenger_count where bus_number = ?", (v1,))
+        count1 = db.c.fetchone()
+        db.c.execute("update passenger_count set count = ? where bus_number = ?", (count1[0] - count[0], v1))
+        db.c.execute('delete from passenger where end_stop = ?', (v2,))
         db.connection.commit()
-    finally:
-        m.showinfo('Update succesful','The current location of the bus has been successfully updated')
-
-
-
-def func1(v):
-    func1.a = v
-    print(func1.a)
-    #return a
 
 
 def call_gui2(v):
@@ -83,38 +83,18 @@ def call_gui2(v):
     t2.title("Conductor UI")
     v1 = StringVar(t2)
     v2 = StringVar(t2)
-    #Label(t2, text="Route Number").grid(row=0, column=0)
     Label(t2, text="Bus Number").grid(row=1, column=0)
     Label(t2, text="Current location").grid(row=2, column=0)
-    #b = Button(t2, text='Next', command=lambda: a.set(v3.get()))
-    #print(a.get())
-    #w3.grid(row=0, column=1)
-    #b.grid(row=0, column=2)
-    #print(func1.a)
     if v == "C6Exp":
-        w1 = OptionMenu(t2, v1, *stopsC6Exp)
+        w1 = OptionMenu(t2, v2, *stopsC6Exp)
         w1.grid(row=2, column=1)
-        w2 = OptionMenu(t2, v2, *C6Exp)
+        w2 = OptionMenu(t2, v1, *C6Exp)
         w2.grid(row=1, column=1)
     elif v == "1 SPL":
-        w1 = OptionMenu(t2, v1, *stops1Spl)
+        w1 = OptionMenu(t2, v2, *stops1Spl)
         w1.grid(row=2, column=1)
-        w2 = OptionMenu(t2, v2, *spl1)
+        w2 = OptionMenu(t2, v1, *spl1)
         w2.grid(row=1, column=1)
-    b1 = Button(t2, text="Submit", command=lambda :update_status(v, v1.get(), v2.get()))
+    b1 = Button(t2, text="Submit", command=lambda: update_status(v, v1.get(), v2.get()))
     b1.grid(row=3, column=1)
-    b2 = Button(t2, text="Close", command=t2.destroy)
-    b2.grid(row=3, column=3)
     t2.mainloop()
-
-'''def func(v):
-    if v == "C6Exp":
-        w1 = OptionMenu(t2, v, *stopsC6Exp)
-        w1.grid(row=1, column=1)
-        w2 = OptionMenu(t2, v, *C6Exp)
-        w2.grid(row=2, column=1)
-    elif v == "1 SPL":
-        w1 = OptionMenu(t2, v, *stops1Spl)
-        w1.grid(row=1, column=1)
-        w2 = OptionMenu(t2, v, *spl1)
-        w2.grid(row=2, column=1)'''
