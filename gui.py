fa122ddba12abe3b76ba83e2f0b9336558c0c120
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-04 02:01:02 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -8,13 +8,10 @@ import database
 def gui(v1):
     t = Tk()
     t.title('BEST Ticketing System')
-    b1 = Button(t, text='Conductor', command=lambda :gui2.call_gui2(v1))
+    b1 = Button(t, text='Conductor', command=lambda :[gui2.call_gui2(v1), t.destroy()])
     b1.grid(row=0)
-    b2 = Button(t, text='Passenger', command=lambda :gui1.call_gui1(v1))
+    b2 = Button(t, text='Passenger', command=lambda :[gui1.call_gui1(v1), t.destroy()])
     b2.grid(row=1)
-    b3 = Button(t, text='Close', command=t.destroy)
-    b3.grid(row=2)
-    #t.mainloop()
 
 
 def form(v):
