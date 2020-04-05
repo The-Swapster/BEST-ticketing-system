e83f2d1187722d5bdad67febc18c4801746c928c
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-06 02:55:22 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,12 +4,21 @@ import sqlite3
 import gui1
 import gui2
 import database
+import webbrowser
+
+
+def open(url):
+    webbrowser.open_new(url)
 
 
 def gui(v1):
-    t = Tk()
+    t = Toplevel()
     t.title('BEST Ticketing System')
     f = tkfont.Font(family='Sans Serif', size=12)
+    img = PhotoImage(file='best_1.png')
+    b3 = Button(t, image=img, command=lambda: open("https://www.bestundertaking.com/in/iis6954.asp?lang=en"))
+    Button.image =img
+    b3.grid(row=0)
     b1 = Button(t, text='Conductor', font=f, bg='red3', fg='white', command=lambda :[gui2.call_gui2(v1), t.destroy()])
     b1.grid(row=1, column=1, padx=120, pady=30)
     b2 = Button(t, text='Passenger', font=f, bg='red3', fg='white', command=lambda :[gui1.call_gui1(v1), t.destroy()])
@@ -23,3 +32,4 @@ def form(v):
 
     except sqlite3.IntegrityError:
         gui(v)
+
