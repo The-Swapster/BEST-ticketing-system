466bb6f2f967786404c9fa39c0b0255e43b389ca
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-05 17:24:54 +0530

    Update gui2.py

--- gui2.py
+++ gui2.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.font as tkfont
 import database as db
 import sqlite3
 
@@ -45,7 +46,7 @@ spl1 = ["MH01CD1234",
         "MH01XY6754",
         "MH01MD3245",
         "MH01AB3848",
-        "MH01BA5705"
+        "MH01BA5707"
         ]
 
 C6Exp = ["MH01RM5018",
@@ -81,20 +82,28 @@ def update_status(v, v1, v2):
 def call_gui2(v):
     t2 = Tk()
     t2.title("Conductor UI")
+    f = tkfont.Font(family='Sans Serif', size=12)
     v1 = StringVar(t2)
     v2 = StringVar(t2)
-    Label(t2, text="Bus Number").grid(row=1, column=0)
-    Label(t2, text="Current location").grid(row=2, column=0)
+    Label(t2, text="Bus Number", font=f, bg='red3', fg='white').grid(row=1, column=0, padx=30, pady=30, ipadx=15)
+    Label(t2, text="Current location", font=f, bg='red3', fg='white').grid(row=2, column=0, padx=30, pady=30)
     if v == "C6Exp":
+        v2.set(stopsC6Exp[0])
         w1 = OptionMenu(t2, v2, *stopsC6Exp)
-        w1.grid(row=2, column=1)
+        w1.config(font=f, bg='red3', fg='white')
+        w1.grid(row=2, column=2)
+        v1.set(C6Exp[0])
         w2 = OptionMenu(t2, v1, *C6Exp)
-        w2.grid(row=1, column=1)
+        w2.config(font=f, bg='red3', fg='white')
+        w2.grid(row=1, column=2)
     elif v == "1 SPL":
+        v2.set(stops1Spl[0])
         w1 = OptionMenu(t2, v2, *stops1Spl)
-        w1.grid(row=2, column=1)
+        w1.config(font=f, bg='red3', fg='white')
+        w1.grid(row=2, column=2, ipadx=5)
+        v1.set(spl1[0])
         w2 = OptionMenu(t2, v1, *spl1)
-        w2.grid(row=1, column=1)
-    b1 = Button(t2, text="Submit", command=lambda: update_status(v, v1.get(), v2.get()))
-    b1.grid(row=3, column=1)
-    t2.mainloop()
+        w2.config(font=f, bg='red3', fg='white')
+        w2.grid(row=1, column=2)
+    b1 = Button(t2, text="Submit", font=f, bg='red3', fg='white', command=lambda: update_status(v, v1.get(), v2.get()))
+    b1.grid(row=3, column=1, padx=30, pady=30, ipadx=45)
