9aa473bcfca3a6a18c7a31003ba9bbc7d720b4e9
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-05 17:22:35 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.font as tkfont
 import sqlite3
 import gui1
 import gui2
@@ -8,10 +9,11 @@ import database
 def gui(v1):
     t = Tk()
     t.title('BEST Ticketing System')
-    b1 = Button(t, text='Conductor', command=lambda :[gui2.call_gui2(v1), t.destroy()])
-    b1.grid(row=0)
-    b2 = Button(t, text='Passenger', command=lambda :[gui1.call_gui1(v1), t.destroy()])
-    b2.grid(row=1)
+    f = tkfont.Font(family='Sans Serif', size=12)
+    b1 = Button(t, text='Conductor', font=f, bg='red3', fg='white', command=lambda :[gui2.call_gui2(v1), t.destroy()])
+    b1.grid(row=0, padx=120, pady=30)
+    b2 = Button(t, text='Passenger', font=f, bg='red3', fg='white', command=lambda :[gui1.call_gui1(v1), t.destroy()])
+    b2.grid(row=1, padx=120, pady=30)
 
 
 def form(v):
