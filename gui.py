eaece8552754f860c839e3b88fcbad6f7971d2d2
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2020-04-06 16:13:50 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -16,12 +16,16 @@ def gui(v1):
     t.title('BEST Ticketing System')
     f = tkfont.Font(family='Sans Serif', size=12)
     img = PhotoImage(file='best_1.png')
+    img1 = PhotoImage(file='driver-removebg-preview.png')
+    img2 = PhotoImage(file='people_1-removebg-preview.png')
     b3 = Button(t, image=img, command=lambda: open("https://www.bestundertaking.com/in/iis6954.asp?lang=en"))
-    Button.image =img
+    Button.image = img
     b3.grid(row=0)
-    b1 = Button(t, text='Conductor', font=f, bg='red3', fg='white', command=lambda :[gui2.call_gui2(v1), t.destroy()])
+    b1 = Button(t, text='Conductor', image=img1, compound=LEFT, font=f, bg='red3', fg='white', command=lambda :[gui2.call_gui2(v1), t.destroy()])
+    b1.image=img1
     b1.grid(row=1, column=1, padx=120, pady=30)
-    b2 = Button(t, text='Passenger', font=f, bg='red3', fg='white', command=lambda :[gui1.call_gui1(v1), t.destroy()])
+    b2 = Button(t, text='Passenger', image=img2, compound=LEFT, font=f, bg='red3', fg='white', command=lambda :[gui1.call_gui1(v1), t.destroy()])
+    b2.image=img2
     b2.grid(row=2, column=1, padx=120, pady=30)
 
 
@@ -32,4 +36,3 @@ def form(v):
 
     except sqlite3.IntegrityError:
         gui(v)
-
